2961060344e7bf6540e021dd5281d36555b00c01
Goh Sheen An <sheen.an.goh@gmail.com>
2020-03-18 11:24:25 +0800

    Updates

---
+++ precache-manifest.69d0c908658af46c2295c1f3eb359b2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa2d6975d098c91f2386dba878449b6",
+    "url": "/react-lotto/index.html"
+  },
+  {
+    "revision": "9b41c65abc458ce94921",
+    "url": "/react-lotto/static/css/main.30c771e7.chunk.css"
+  },
+  {
+    "revision": "62691667aa33c5303aec",
+    "url": "/react-lotto/static/js/2.155f86e6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-lotto/static/js/2.155f86e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b41c65abc458ce94921",
+    "url": "/react-lotto/static/js/main.fb8c81b9.chunk.js"
+  },
+  {
+    "revision": "911a5b32b6f5f6acd51f",
+    "url": "/react-lotto/static/js/runtime-main.6cf20352.js"
+  }
+]);
